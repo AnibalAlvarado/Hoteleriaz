56576aec5bc2842e580eb27439ca546bf23e0783
JhonnCorredor <jhoncorredormedina29@gmail.com>
2025-03-06 07:44:52 -0500

    Front Back and MER

--- HoteleriazFrontEnd/src/app/components/hoteles/hoteles.component.ts
+++ HoteleriazFrontEnd/src/app/components/hoteles/hoteles.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-hoteles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCardModule],
   templateUrl: './hoteles.component.html',
   styleUrl: './hoteles.component.scss'
 })
--- HoteleriazFrontEnd/src/app/pages/landing-page/landing-page.component.ts
+++ HoteleriazFrontEnd/src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HotelesComponent } from '../../components/hoteles/hoteles.component';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HotelesComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
